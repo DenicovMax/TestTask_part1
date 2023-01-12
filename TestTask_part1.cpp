8f5d0a6d51c1eef6762e13c757415e1335426073
DenicovMax <121465303+DenicovMax@users.noreply.github.com>
2023-01-13 00:35:33 +0400

    Initial commit

---
+++ TestTask_part1.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+std::string convert_string(const std::string& input)
+{
+    std::unordered_map<char, int> char_counts;
+    std::string output;
+
+    for (char c : input) {
+        char_counts[std::tolower(c)]++;
+    }
+
+    for (char c : input) {
+        c = std::tolower(c);
+        if (char_counts[c] == 1) {
+            output += "(";
+        }
+        else {
+            output += ")";
+        }
+    }
+
+    return output;
+}
+
+int main()
+{
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::string output = convert_string(input);
+    std::cout << output << std::endl;
+
+    return 0;
+}
